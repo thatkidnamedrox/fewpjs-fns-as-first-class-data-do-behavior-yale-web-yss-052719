d3e3fe196be42bae6d18634c9173c6eec58816d2
Roxanne Harris <roxanne.harris@yale.edu>
2019-08-02 23:41:55 -0400

    initial commit

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(str){
+  let [hours,] = str.split(":");
+  let h = parseInt(hours);
+  if (h < 12) {
+    return "Good Morning";
+  } else if (h < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(content) {
+  document.getElementById("greeting").innerText = content;
+}
